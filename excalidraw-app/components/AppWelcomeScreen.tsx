ca9448b28cfe7c11931e3ffa8829b31a405180ba
June_qwq <107529938+lemonorangeapple@users.noreply.github.com>
2025-09-23 10:40:56 +0800

    Update AppWelcomeScreen.tsx

--- excalidraw-app/components/AppWelcomeScreen.tsx
+++ excalidraw-app/components/AppWelcomeScreen.tsx
@@ -12,28 +12,7 @@ export const AppWelcomeScreen: React.FC<{
   const { t } = useI18n();
   let headingContent;
 
-  if (isExcalidrawPlusSignedUser) {
-    headingContent = t("welcomeScreen.app.center_heading_plus")
-      .split(/(Excalidraw\+)/)
-      .map((bit, idx) => {
-        if (bit === "Excalidraw+") {
-          return (
-            <a
-              style={{ pointerEvents: POINTER_EVENTS.inheritFromUI }}
-              href={`${
-                import.meta.env.VITE_APP_PLUS_APP
-              }?utm_source=excalidraw&utm_medium=app&utm_content=welcomeScreenSignedInUser`}
-              key={idx}
-            >
-              Excalidraw+
-            </a>
-          );
-        }
-        return bit;
-      });
-  } else {
     headingContent = t("welcomeScreen.app.center_heading");
-  }
 
   return (
     <WelcomeScreen>
